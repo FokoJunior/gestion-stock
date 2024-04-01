441c35e290a63098cab71e7cc9993fd3377eae2d
Foko Junior <122606323+FokoJunior@users.noreply.github.com>
2024-04-01 10:35:40 +0100

    Add files via upload

---
+++ application/views/templates/footer.php
@@ -0,0 +1,16 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>F_Junior</b>
+    </div>
+    <strong>Copyright &copy; <?php echo date('Y') ?>.</strong> Tous les droits sont reservees.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
